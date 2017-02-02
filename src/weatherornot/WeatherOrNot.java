b45bc9c9af51026ea3041ecc82b9153593492a3a
Zhewei <Zhewei@104.39.93.105>
2017-02-02 14:58:01 -0500

    Get user's IP

--- src/weatherornot/WeatherOrNot.java
+++ src/weatherornot/WeatherOrNot.java
@@ -7,7 +7,7 @@ package weatherornot;
 
 import com.github.dvdme.ForecastIOLib.*;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+
 /**
  *
  * @author Zhewei
@@ -17,20 +17,26 @@ public class WeatherOrNot {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws UnknownHostException {
-        // state college
-        ForecastIO forecast = new ForecastIO("40.7948376","-77.8653124","9811b7c9d35ea099b80118df438269e2");
-        System.out.println(forecast.getCurrently().toString());
-        // access by key
-        String summary = forecast.getCurrently().get("summary").toString();
-        System.out.println(removeQuotes(summary));
-        // IP Address
-        InetAddress IP=InetAddress.getLocalHost();
-        System.out.println(IP.getHostAddress().toString());
+    public static void main(String[] args) {
+        try {
+            // state college
+            ForecastIO forecast = new ForecastIO("40.7948376", "-77.8653124", "9811b7c9d35ea099b80118df438269e2");
+            System.out.println(forecast.getCurrently().toString());
+            // access by key
+            String summary = forecast.getCurrently().get("summary").toString();
+            System.out.println(removeQuotes(summary));
+            // IP Address
+            InetAddress IP;
+            IP = InetAddress.getLocalHost();
+            System.out.println(IP.getHostAddress().toString());
+        } catch (Exception ex) {
+            System.out.println("You are offline.");
+        }
+
     }
-    
+
     public static String removeQuotes(String thing) {
-        return thing.substring(1,thing.length()-1);
+        return thing.substring(1, thing.length() - 1);
     }
-    
+
 }

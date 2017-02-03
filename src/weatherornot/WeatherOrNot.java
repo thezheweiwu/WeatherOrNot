f55af13b9445d70d91a8ac0daee38fbcfaef5120
Zhewei <Zhewei@104.39.93.105>
2017-02-03 14:12:54 -0500

    improve IP finding

--- src/weatherornot/WeatherOrNot.java
+++ src/weatherornot/WeatherOrNot.java
@@ -10,8 +10,11 @@ import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.model.CityResponse;
 import com.maxmind.geoip2.record.City;
 import com.maxmind.geoip2.record.Subdivision;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
 
 /**
  *
@@ -24,24 +27,29 @@ public class WeatherOrNot {
      */
     public static void main(String[] args) {
         try {
-            File database = new File("src/WeatherOrNot/GeoLite2-City.mmdb");
+            File database = new File("db/GeoLite2-City.mmdb");
             DatabaseReader reader = new DatabaseReader.Builder(database).build();
 
-            InetAddress IP = InetAddress.getLocalHost();
+            // IP Address
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+
+            String ipString = in.readLine();
+            InetAddress IP  =  InetAddress.getByName(ipString);
             CityResponse response = reader.city(IP);
             com.maxmind.geoip2.record.Location location = response.getLocation();
             Subdivision subdivision = response.getMostSpecificSubdivision();
             City city = response.getCity();
-            System.out.println(city.getName()+", "+subdivision.getIsoCode()); // 'MN'
+            System.out.print(city.getName() + ", " + subdivision.getIsoCode() + ": "); // 'MN'
             // state college
             ForecastIO forecast = new ForecastIO(location.getLatitude().toString(), location.getLongitude().toString(), "9811b7c9d35ea099b80118df438269e2");
-            System.out.println(forecast.getCurrently().toString());
+            System.out.println(forecast.getCurrently());
             // access by key
-            String summary = forecast.getCurrently().get("summary").toString();
-            System.out.println(removeQuotes(summary));
-            // IP Address
+            //String summary = forecast.getCurrently().get("summary").toString();
+            //System.out.println(removeQuotes(summary));
         } catch (Exception ex) {
-            System.out.println("You are offline.");
+            System.out.println(ex);
         }
 
     }

ef19bd5dca96d870018c802b0879471756ad5919
Zhewei <Zhewei@104.39.93.105>
2017-02-21 15:39:04 -0500

    remove centering

--- src/weatherornot/LocationPanel.java
+++ src/weatherornot/LocationPanel.java
@@ -20,28 +20,33 @@ import javax.swing.JTextField;
         
 public class LocationPanel extends JPanel{
     private final JLabel directions;
-    JTextField zipcodeField;
+    private JTextField zipcodeField;
     private final JButton submit; 
     
     LocationPanel() {
         super();
         setBackground(new Color(150,192,206));
-        directions = new JLabel();
-        directions.setText("Input a zipcode to select a new location");
+        directions = new JLabel("Input a zipcode to select a new location");
         directions.setFont(new Font("SansSerif", Font.BOLD, 16));
         directions.setForeground(Color.white);
-        directions.setHorizontalAlignment(JLabel.CENTER);
-        directions.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
         add(directions);
-        zipcodeField = new JTextField("Zip", 5);
-        zipcodeField.setHorizontalAlignment(JTextField.CENTER);
+        zipcodeField = new JTextField("", 5);
         zipcodeField.setFont(new Font("SansSerif", Font.BOLD, 16));
         zipcodeField.setForeground(new Color(116,130,143));
         add(zipcodeField);
         submit = new JButton("Submit");
         submit.setBackground(new Color(194,91,86));
         submit.setForeground(Color.white);
-        submit.setHorizontalAlignment(JButton.CENTER);
         add(submit);
     }
+
+    public JTextField getZipcodeField() {
+        return zipcodeField;
+    }
+
+    public JButton getSubmit() {
+        return submit;
+    }
+    
+    
 }
